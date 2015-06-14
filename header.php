ec29e191789ba31546d13d9ae618b77e15cb96b0
Roy Sivan <roy.x.sivan.-ND@disney.com>
2015-06-14 01:00:44 -0700

    RWD for L shape

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-		<meta name="author" content="DTSS-DDM">
+		<meta name="author" content="WPCROWD">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="/favicon.ico">
 		<link rel="apple-touch-icon" href="/favicon.png">

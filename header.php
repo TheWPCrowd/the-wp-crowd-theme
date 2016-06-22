afaa63121eebf1608628c9761c948261b9848602
Roy Sivan <roy@guavaworks.com>
2016-06-22 13:52:42 -0700

    changing header

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-5">
-						<a href="<?php echo get_bloginfo('wpurl'); ?>">
+						<a href="<?php echo get_bloginfo('url'); ?>">
 							<img src="<?php echo get_template_directory_uri().'/img/logo.png'; ?>" alt="The WP Crowd WordPress Podcast" class="img-responsive" />
 						</a>
 					</div>

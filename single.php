475cf6b61f3fb6c4ca893e8e2bf07790210ba907
Christopher Ross <cross@thisismyurl.com>
2016-06-26 17:44:36 -0300

    Code cleanup, mostly spacing and improving readability

--- single.php
+++ single.php
@@ -8,113 +8,130 @@ the_post();
 		<div class="col-sm-8 content">
 			<h2 class="single-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>"><?php the_title(); ?></a></h2>
 			<div class="single-top">
-				<?php
-						if( 'podcast' == get_post_type() ) {
-						
-							$people_terms = wp_get_post_terms( $post->ID, 'people', array( 'fields' => 'all' ) );
-							$podcasters = array();
+			<?php
+			if( 'podcast' == get_post_type() ) {
+			
+				$people_terms = wp_get_post_terms( $post->ID, 'people', array( 'fields' => 'all' ) );
+				$podcasters = array();
 
-							if( ! empty( $people_terms ) ) {
-								foreach( $people_terms as $person ) {
-									if( function_exists( 'get_field' ) ) {
-										$associated_user = get_field( 'associated_user', $person->taxonomy . '_' . $person->term_id );
-										if( $associated_user ) {
-											$podcasters[] = $associated_user['ID'];
-										}
-									}
-								}
+				if( ! empty( $people_terms ) ) {
+					foreach( $people_terms as $person ) {
+						if( function_exists( 'get_field' ) ) {
+							$associated_user = get_field( 'associated_user', $person->taxonomy . '_' . $person->term_id );
+							if( $associated_user ) {
+								$podcasters[] = $associated_user['ID'];
 							}
-								?>
-								<div class="embed-responsive embed-responsive-16by9"><iframe id="podcast" class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo get_field( 'youtube_video_id', $post->ID); ?>" frameborder="0" allowfullscreen></iframe></div>
-								<div class="air-date text-right"><strong><?php _e( 'Aired:', 'wpcrowd' );?></strong> <?php if( function_exists( 'get_field' ) ) { echo get_field( 'air_date' ); } ?></div>
-								<div class="podcast-people">
-										<strong><?php _e( 'In This Episode', 'wpcrowd' );?></strong>
-										<?php
-											if(! empty( $podcasters) && is_array( $podcasters) ) {
-												foreach( $podcasters as $user ) { ?>
-													<a href="<?php echo get_author_posts_url( $user ); ?>">
-													<?php
-														echo get_avatar( $user, 300, '', __( 'The WP Crowd', 'wpcrowd' ), array( 'class' => 'img-responsive' ) );
-														$usermeta = get_user_meta( $user );
-														echo $usermeta['first_name'][0] . ' ' . $usermeta['last_name'][0];
-													?>
-													</a>
-										<?php
-												}
-											}
-										?>
-										</div>
-										<div class="categories">
-												<strong><?php _e( 'Topics:', 'wpcrowd' );?></strong>
-												<?php $topics = wp_get_post_terms( $post->ID, 'topics', array( 'fields' => 'all' ) );
-												if(is_array( $topics) ) {
-												?>
-												<ul>
-														<?php foreach( $topics as $topic ) {
-																echo '<li><a href="/topics/' . $topic->slug . '" class="person">' . $topic->name . '</a></li>';
-														} ?>
-												</ul>
-												<?php } ?>
-										</div>
+						}
+					}
+				}
+				?>
+				<div class="embed-responsive embed-responsive-16by9"><iframe id="podcast" class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo get_field( 'youtube_video_id', $post->ID); ?>" frameborder="0" allowfullscreen></iframe></div>
+				<div class="air-date text-right"><strong><?php _e( 'Aired:', 'wpcrowd' );?></strong> <?php if( function_exists( 'get_field' ) ) { echo get_field( 'air_date' ); } ?></div>
+				
+
+				<div class="podcast-people">
+					<strong><?php _e( 'In This Episode', 'wpcrowd' );?></strong>
 					<?php
+					if ( ! empty( $podcasters ) && is_array( $podcasters ) ) {
+						
+						foreach( $podcasters as $user ) { 
 
-					} else {
-						the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) );
+							$usermeta = get_user_meta( $user );
+							$username = $usermeta['first_name'][0] . ' ' . $usermeta['last_name'][0];
 
+						?>
+							<a href="<?php echo get_author_posts_url( $user ); ?>" title="<?php echo esc_attr( $username ) );?>">
+							<?php
+								echo get_avatar( $user, 300, '', __( 'The WP Crowd', 'wpcrowd' ), array( 'class' => 'img-responsive' ) );
+								echo $username;
+							?>
+							</a>
+						<?php
+
+						} /* foreach */
+
+					} /* if */
 					?>
-						<div class="author-meta row">
-							<div class="col-xs-2 author-avatar text-center">
-								<?php echo get_avatar( $post->post_author, 300, '', __( 'The WP Crowd', 'wpcrowd' ), array( 'class' => 'img-responsive' ) ); ?>
-							</div>
-							<div class="col-xs-7">
+				</div>
 
-								<h3><?php echo get_the_author_meta( 'display_name' ) ?></h3>
-								<?php if( function_exists( 'get_field' ) && get_field( 'title', 'user_' . $post->post_author ) ) : ?>
-									<h5>
-									<?php
-										$title = get_field( 'title', 'user_' . $post->post_author );
-										$title = str_replace( '-', ' ', $title );
-										$title = str_replace( '_', ' ', $title );
-										echo $title;
-									?>
-									</h5>
 
-								<?php endif;
+				<div class="categories">
+					<strong><?php _e( 'Topics:', 'wpcrowd' );?></strong>
+					<?php $topics = wp_get_post_terms( $post->ID, 'topics', array( 'fields' => 'all' ) );
+					if( is_array( $topics ) ) {
+					?>
+					<ul>
+							<?php 
+							foreach( $topics as $topic ) {
+								echo '<li><a href="/topics/' . $topic->slug . '" class="person" title="' . esc_attr( $topic->name ) . '">' . $topic->name . '</a></li>';
+							}
+							?>
+					</ul>
+					<?php } ?>
+				</div>
+			<?php
 
-								if( function_exists( 'wpcrowd_author_follow' ) ) {
-									wpcrowd_author_follow();
-								} else {
-									$user = get_user_by( 'id', $post->post_author );
-									if( function_exists( 'get_field' ) && get_field( 'biography', 'user_' . $user->ID ) ) : ?>
-											<a href="<?php echo get_author_posts_url( $user->ID ) ?>" class="bio circle">
-													<i><?php _e( 'BIO', 'wpcrowd' );?></i>
-											</a>
-									<?php endif;
-									if( function_exists( 'get_field' ) && get_field( 'twitter_handle', 'user_' . $user->ID ) ) : ?>
-											<a href="https://twitter.com/' <?php echo get_field( 'twitter_handle', 'user_' . $user->ID ) ?>" target="_blank" class="circle">
-													<i class="fa fa-twitter" aria-hidden="true"></i>
-											</a>
-									<?php endif;
-									if( function_exists( 'get_field' ) && get_field( 'facebook_url', 'user_' . $user->ID ) ) : ?>
-									<a href="<?php echo get_field( 'facebook_url', 'user_' . $user->ID ) ?>" target="_blank" class="circle">
-													<i class="fa fa-facebook" aria-hidden="true"></i>
-											</a>
-									<?php endif;
-									if( $user->user_url ) : ?>
-											<a href="<?php $user->user_url ?>" target="_blank" class="circle">
-													<i class="fa fa-home" aria-hidden="true"></i>
-											</a>
-									<?php endif;
+			} else {
+				the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) );
 
-							}?>
-							</div>
-							<div class="col-xs-3 date"><strong>Published: </strong><?php echo get_the_date( 'F j, Y' ) ?></div>
-						</div>
-						<div class="categories">
-							<strong><?php _e( 'Topics:', 'wpcrowd' );?></strong>
-							<?php echo get_the_category_list() ?>
-						</div>
-				<?php 	} ?>
+			?>
+				<div class="author-meta row">
+					<div class="col-xs-2 author-avatar text-center">
+						<?php echo get_avatar( $post->post_author, 300, '', __( 'The WP Crowd', 'wpcrowd' ), array( 'class' => 'img-responsive' ) ); ?>
+					</div>
+					<div class="col-xs-7">
+
+						<h3><?php echo get_the_author_meta( 'display_name' ) ?></h3>
+						<?php if( function_exists( 'get_field' ) && get_field( 'title', 'user_' . $post->post_author ) ) : ?>
+							<h5>
+							<?php
+								$title = get_field( 'title', 'user_' . $post->post_author );
+								$title = str_replace( '-', ' ', $title );
+								$title = str_replace( '_', ' ', $title );
+								echo $title;
+							?>
+							</h5>
+
+						<?php endif;
+
+						if( function_exists( 'wpcrowd_author_follow' ) ) {
+							wpcrowd_author_follow();
+						} else {
+							$user = get_user_by( 'id', $post->post_author );
+							if( function_exists( 'get_field' ) && get_field( 'biography', 'user_' . $user->ID ) ) : ?>
+									<a href="<?php echo get_author_posts_url( $user->ID ) ?>" class="bio circle">
+											<i><?php _e( 'BIO', 'wpcrowd' );?></i>
+									</a>
+							<?php endif;
+							if( function_exists( 'get_field' ) && get_field( 'twitter_handle', 'user_' . $user->ID ) ) : ?>
+									<a href="https://twitter.com/' <?php echo get_field( 'twitter_handle', 'user_' . $user->ID ) ?>" target="_blank" class="circle">
+											<i class="fa fa-twitter" aria-hidden="true"></i>
+									</a>
+							<?php endif;
+							if( function_exists( 'get_field' ) && get_field( 'facebook_url', 'user_' . $user->ID ) ) : ?>
+							<a href="<?php echo get_field( 'facebook_url', 'user_' . $user->ID ) ?>" target="_blank" class="circle">
+											<i class="fa fa-facebook" aria-hidden="true"></i>
+									</a>
+							<?php endif;
+							if( $user->user_url ) : ?>
+									<a href="<?php $user->user_url ?>" target="_blank" class="circle">
+											<i class="fa fa-home" aria-hidden="true"></i>
+									</a>
+							<?php endif;
+
+					}?>
+					</div>
+					<div class="col-xs-3 date"><strong><?php _e( 'Published:', 'wpcrowd' );?> </strong><?php echo get_the_date( 'F j, Y' ) ?></div>
+				</div>
+				<div class="categories">
+					<strong><?php _e( 'Topics:', 'wpcrowd' );?></strong>
+					<?php echo get_the_category_list() ?>
+				</div>
+		<?php 	} ?>
+
+
+
+		
 			</div>
 			<div class="content-container">
 				<?php

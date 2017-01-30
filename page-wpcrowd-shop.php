5f8d3e03290e6b101b63560486a380b495a81ef7
Roy Sivan <roy.sivan@disney.com>
2017-01-30 06:54:55 -0800

    adding in shop page

---
+++ page-wpcrowd-shop.php
@@ -0,0 +1,45 @@
+<?php
+get_header();
+the_post();
+?>
+
+<div class="container single-container">
+	<div class="row">
+
+
+		<div class="col-sm-8 content">
+
+			<h2 class="single-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>"><?php the_title(); ?></a></h2>
+
+			<?php if ( has_post_thumbnail() ) { ?>
+			<div class="single-top">
+				<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) ); ?>
+			</div>
+			<?php } ?>
+			<div class="content-container">
+				<div id="myShop"><a href="//shop.spreadshirt.com/thewpcrowd">thewpcrowd</a></div>
+
+				<script>
+					var spread_shop_config = {
+						shopName: 'thewpcrowd',
+						locale: 'us_US',
+						prefix: '//shop.spreadshirt.com',
+						baseId: 'myShop'
+					};
+				</script>
+
+				<script type="text/javascript" src="//shop.spreadshirt.com/shopfiles/shopclient/shopclient.nocache.js"></script>
+			</div>
+
+		</div>
+
+
+		<div class="col-sm-4 sidebar">
+			<?php dynamic_sidebar( 'single-sidebar' ); ?>
+		</div>
+
+
+	</div>
+</div>
+
+<?php get_footer();

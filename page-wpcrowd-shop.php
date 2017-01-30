cd6fbcf05b0ee2588d78be0adffeabb625f134e8
Roy Sivan <roy.sivan@disney.com>
2017-01-30 06:55:52 -0800

    Shop Page Edits

--- page-wpcrowd-shop.php
+++ page-wpcrowd-shop.php
@@ -8,14 +8,6 @@ the_post();
 
 
 		<div class="col-sm-8 content">
-
-			<h2 class="single-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_title() ); ?>"><?php the_title(); ?></a></h2>
-
-			<?php if ( has_post_thumbnail() ) { ?>
-			<div class="single-top">
-				<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive' ) ); ?>
-			</div>
-			<?php } ?>
 			<div class="content-container">
 				<div id="myShop"><a href="//shop.spreadshirt.com/thewpcrowd">thewpcrowd</a></div>
 
